22a0adfd9d41cb01585736a6d77bdd28246fd264
99liza99 <liza.owchinnikowa@gmail.com>
2022-07-22 01:53:48 +0300

    firts commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialExampleModule} from '../material.module';
 
 import { AppComponent } from './app.component';
-import { DoctorsComponent } from './doctors/doctors.component';
+import { DoctorsComponent, DialogCompoent } from './doctors/doctors.component';
 import { DoctorComponent } from './doctor/doctor.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DoctorsComponent,
-    DoctorComponent
+    DoctorComponent,
+    DialogCompoent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/doctors/doctors.component.ts
+++ src/app/doctors/doctors.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-doctors',
   templateUrl: './doctors.component.html',
-  styleUrls: ['./doctors.component.css']
+  styleUrls: ['./doctors.component.css'],
 })
 export class DoctorsComponent implements OnInit {
   title: string = 'List Doctors';
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  openDialog(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string
+  ): void {
+    this.dialog.open(DialogCompoent, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
   }
+}
 
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+})
+export class DialogCompoent {
+  constructor(public dialogRef: MatDialogRef<DialogCompoent>) {}
 }
